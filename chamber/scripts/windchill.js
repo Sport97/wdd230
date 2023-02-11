e7c67918a1f6532385d3bcfa2d6f6e11b542baa4
Sport97 <por21022@byui.edu>
2023-02-10 23:29:15 -0600

    Responsive Design

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,18 @@
+const temp = document.querySelector("#temp");
+const speed = document.querySelector("#speed");
+
+function windChill(number1, number2){
+    return 35.74 + 0.6215 * (number1) - 35.75 * (number2 ** 0.16) + 0.4275 * (number1) * (number2 ** 0.16);
+};
+
+document.querySelector("#calculate").addEventListener("click",  function() {
+    if (temp.value <= 50 & speed.value >= 3) {
+        const temp = document.querySelector("#temp").value;
+        const speed = document.querySelector("#speed").value;
+        const chill = windChill(parseInt(temp), parseInt(speed));
+        return document.querySelector("#chill").value = Math.round(chill);
+    }
+    else {
+        return document.querySelector("#chill").value = "N/A"
+    }
+});
