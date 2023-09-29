b13fd3a546f9f0c427a70576cf3e57e22338f362
Sport97 <por21022@byui.edu>
2023-09-28 22:13:45 -0500

    Lesson 3 Learning Activities

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,5 +2,12 @@ const currentDate = new Date(document.lastModified); // gets todays date based o
 const year = currentDate.getFullYear();
 const footerYear = `© ${year} | Stephen Port | Minnesota`;
 const lastModified = `Last Modified: ${currentDate}`;
+const mainnav = document.querySelector(".navigation")
+const hambutton = document.querySelector("#menu");
+
 document.querySelector("#footerYear").textContent = footerYear
 document.querySelector("#lastModified").textContent = lastModified
+
+hambutton.addEventListener("click", function() {
+	mainnav.classList.toggle("responsive")
+}, false);
