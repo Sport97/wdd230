db2c8f5c2419516ad035a0b3850585bb57e47dea
Sport97 <por21022@byui.edu>
2024-04-25 17:03:44 -0500

    week 01

---
+++ scripts/getDates.js
@@ -0,0 +1,7 @@
+const currentDate = new Date(document.lastModified); // gets todays date based on computer system
+const year = currentDate.getFullYear();
+const footerYear = `© ${year} | Stephen Port | Minnesota`;
+const lastModified = `Last Modified: ${currentDate}`;
+
+document.querySelector("#footerYear").textContent = footerYear
+document.querySelector("#lastModified").textContent = lastModified
