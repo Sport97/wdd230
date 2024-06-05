6d22e61b75e3d83ebfbf8c6dcbf7e3ca655638cb
Sport97 <por21022@byui.edu>
2024-06-05 17:17:22 -0500

    week07

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,24 +1,25 @@
 const currentDay = new Date().getDay();
 const home = document.querySelector("#home-page");
 const banner = document.querySelector("#banner");
-const closeBanner = document.querySelector("#close-banner");
 
 function displayBanner() {
     if (currentDay === 1 || currentDay === 2 || currentDay === 3) {
         const message = document.createElement("p");
-		message.innerHTML = "Meet and Greet on Wednesday at 7:00 p.m";
+		message.textContent = "Meet and Greet on Wednesday at 7:00 p.m.";
+
+        const closeBanner = document.createElement("button");
+        closeBanner.textContent = "✖";
+        closeBanner.setAttribute("id", "close-banner");
+
         banner.appendChild(message);
+        banner.appendChild(closeBanner);
+
+        closeBanner.addEventListener("click", function() {
+            home.classList.toggle("close")
+            closeBanner.classList.toggle("close")
+            banner.classList.toggle("close")
+        }, false);
     }
 }
-
-closeBanner.addEventListener("click", () => {
-    banner.style.display = "none";
-});
-
-closeBanner.addEventListener("click", function() {
-	home.classList.toggle("close")
-    closeBanner.classList.toggle("close")
-    banner.classList.toggle("close")
-}, false);
 
 displayBanner();
